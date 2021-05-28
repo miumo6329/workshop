c0ff3b9f649e35003c6ad50f4b07e3cce05347e8
miumo <miumo2020@gmail.com>
2021-05-28 16:20:40 +0900

    ポート指定通信確認一括処理スクリプト作成

---
+++ powershell/test_netconnection/test_netconnection.ps1
@@ -0,0 +1,9 @@
+﻿$path = Split-Path -Parent $MyInvocation.MyCommand.Path
+Set-Location $path
+$csv = Import-Csv "address.csv" -Encoding ASCII
+foreach($row in $csv){
+    $host_ip = $row.host
+    foreach($port in $row.port.Split(",")){
+        Test-NetConnection $host_ip -Port $port
+    }
+}
