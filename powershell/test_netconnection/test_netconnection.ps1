a411567bdb779df0a16ef61cd57ab6b82a15ba01
miumo <miumo2020@gmail.com>
2021-05-31 22:36:16 +0900

    結果テキストファイル出力

--- powershell/test_netconnection/test_netconnection.ps1
+++ powershell/test_netconnection/test_netconnection.ps1
@@ -1,9 +1,11 @@
 ﻿$path = Split-Path -Parent $MyInvocation.MyCommand.Path
 Set-Location $path
 $csv = Import-Csv "address.csv" -Encoding ASCII
+Start-Transcript "result.txt"
 foreach($row in $csv){
     $host_ip = $row.host
     foreach($port in $row.port.Split(",")){
         Test-NetConnection $host_ip -Port $port
     }
 }
+Stop-Transcript
