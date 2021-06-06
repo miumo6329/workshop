2f00a50c4cc21140a978cb49aec911c060f3876e
miumo <miumo2020@gmail.com>
2021-06-06 14:49:48 +0900

    途中進捗コミット

---
+++ guitar_code_palette/guitar.js
@@ -0,0 +1,6 @@
+// let strings = document.getElementsByClassName("string");
+// for (let s of strings) {
+//     s.addEventListener("mouseover", function(event){
+//         s.style.backgroundColor = "red";
+//     });
+// };
